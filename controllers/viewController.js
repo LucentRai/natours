3a0c6a1f2bd647ebe21ac9aebe6503f002cbb45a
Lucent Rai <lucentthulung@gmail.com>
2023-05-13 21:57:28 +0545

    Tour Route view Created

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,10 +1,11 @@
 const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
+const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/AppError');
 
 async function getOverview(request, response, next){
 	const tours = await Tour.find();
-	console.log(typeof(tours));
-
 	response.status(200)
 		.render('overview', {
 			title: 'All Tours',
@@ -12,14 +13,20 @@ async function getOverview(request, response, next){
 		});
 }
 
-function getTour(request, response, next){
-	response.status(200)
-		.render('tour', {
-			title: 'Forest Hiker'
-		});
+async function getTour(request, response, next){
+	const tour = await Tour.findOne({slug: request.params.slug}).populate({
+		path: 'reviews',	// name of virtual populate path
+		fields: 'review rating user' // fields to populate
+	});
+
+	if(!tour){
+		return next(new AppError(`This page does not exist`, 404));
+	}
+
+	response.status(200).render('tour', {tour});
 }
 
 module.exports = {
 	getOverview: catchAsync(getOverview),
-	getTour
+	getTour: catchAsync(getTour)
 };
--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -4,6 +4,6 @@ const viewController = require('../controllers/viewController');
 const viewRouter = express.Router();
 
 viewRouter.get('/', viewController.getOverview);
-viewRouter.get('/tour', viewController.getTour);
+viewRouter.get('/tour/:slug', viewController.getTour);
 
 module.exports = viewRouter;
